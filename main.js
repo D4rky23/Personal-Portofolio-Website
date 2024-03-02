e437298cec23e04952e086bcbfb05b2ad2bbb297
D4rky23 <andrei_alexandru2305@yahoo.com>
2024-03-02 20:06:02 +0200

    Typing Effect + Mobile View + Home Section

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ MenuButton.addEventListener('click', function(e){
     document.querySelector('body').classList.toggle('mobile-nav-active');
     this.classList.toggle('fa-xmark')
 })
+
+// Typing Effect
+let typed = new Typed('#auto-input', {
+    strings: ['Full-Stack Developer!','Web Designer!', 'Student!'],
+    typeSpeed: 90,
+    backSpeed: 90,
+    backDelay: 100,
+    loop: true,
+})
